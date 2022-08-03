6dc12b451867bef152f276a912191d1cebee1ae9
Choi jaemin <panzzang518@naver.com>
2022-08-03 17:08:20 +0900

    13 Conditional Rendering v-show (초기 비용 큼) Vs v-if (토글 시 비용 큼)

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,24 @@
 <template>
   <div class="container">
     <h2>To-DO List</h2>
-    <form
-        @submit.prevent="onSubmit"
-        class="d-flex">
-      <div class="flex-grow-1 mr-2">
-        <input
+    <form @submit.prevent="onSubmit">
+      <div class="d-flex">
+        <div class="flex-grow-1">
+          <input
             class="form-control"
             type="text"
             v-model="todo">
       </div>
-      <div>
-        <button
-            class="btn btn-primary"
-            @click="onSubmit">
-          Add
-        </button>
+        <div>
+          <button
+              class="btn btn-primary"
+              @click="onSubmit">
+            Add
+          </button>
+        </div>
       </div>
     </form>
+    <div v-show="hasError" style="color: red">This field cannot be empty</div>
     <div
         v-for="todo in todos"
         :key="todo.id"
@@ -35,7 +36,9 @@ import {ref} from "vue";
 
 export default {
   setup() {
+    const toggle = ref(false);
     const todo = ref('최재민');
+    const hasError = ref(false);
     const todos = ref([
       {
         id:1, subject: "휴대폰 사기",
@@ -45,19 +48,32 @@ export default {
       }
     ]);
 
-
     const onSubmit = () => {
-      console.log(todo.value)
-      todos.value.push({
-        id: Date.now(),
-        subject: todo.value,
-      })
+      if (todo.value === '') {
+        hasError.value = true;
+      }
+      else{
+        console.log(todo.value);
+        todos.value.push({
+          id: Date.now(),
+          subject: todo.value,
+        });
+        hasError.value = false;
+      }
+    };
+
+    const onToggle = () => {
+      toggle.value = !toggle.value;
     };
 
     return {
+      hasError,
+      toggle,
       todo,
       todos,
       onSubmit,
+      onToggle,
+
     };
   }
 }
